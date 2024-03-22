90923e2ddd5200ec8cb4e1a6135be870ccd12df4
Nikhar Tale <nikhar.tale@zehntech.com>
2024-03-23 00:45:06 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:event_explorer/bloc/event_bloc.dart';
 import 'package:event_explorer/screens/home.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'services/app_theme.dart';
 
-void main() {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Ensure Flutter binding is initialized
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
@@ -25,7 +29,7 @@ class App extends StatelessWidget {
         theme: appTheme,
         debugShowCheckedModeBanner: false,
         title: 'Your App Title',
-        home: HomeScreen(),
+        home: const HomeScreen(),
       ),
     );
   }
